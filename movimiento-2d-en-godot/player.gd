12835cbe31d7d76e2c363167aca0874d5883bfa3
Rayvet24 <elias.ivan.rodriguez2004@gmail.com>
2026-04-13 02:22:17 -0300

    Arreglo de nodo y movimiento lateral hecho
    Uno de los cambios principales fue cambiar la forma de usar las animaciones al cambiar Sprite2D por AnimatedSprite2D, esto permitió reducir mucho el código y ya lo deja configurado las animaciones fuera del código. Movimiento lateral de Player, seguramente se pueda optimizar pero mi mayor obstaculo fue evitar el problema de que las animaciones no se reproducían bien, por diferentes razones. Al final cambió la estructura del código a una mucho más legible y mas reducida, sigue teniendo un problema que se mantiene el movimiento si se cambia de rumbo en el aire.

--- movimiento-2d-en-godot/player.gd
+++ movimiento-2d-en-godot/player.gd
@@ -1,62 +1,56 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 20
 @export var gravity = 980
-@export var min_jump_foce = -150
-@export var max_jump_foce = -250
-@export var jump_aceleration = -400
+@export var min_jump_force = -100
+@export var max_jump_force = -500
+@export var jump_aceleration = -250
 
-var jumping = false
+var jumping: bool
+
 #func _ready() -> void:
 #	pass
 
+# control + k comentar varias lineas
+
 func _physics_process(delta) -> void:
-	velocity.x = 0
-	#velocity.y = 0
 	
-	if not is_on_floor(): #gravedad
+	if not is_on_floor(): #si no esta en el piso se activa gravedad
 		velocity.y += gravity * delta 
 	else:
 		velocity.y = 0
 		jumping = false
 	
-	if is_on_floor():
-		$Sprite.frame = 0
 	
-	if Input.is_action_pressed("move_left") and is_on_floor(): #mover izq
-		velocity.x -= speed
-		$Sprite.flip_h = true
-		$Sprite.frame = 12
-		#await get_tree().create_timer(0.2).timeout
-		#$Sprite.frame = 13
-		#$Sprite.frame = 14
-		#$Sprite.frame = 15
-	if Input.is_action_pressed("move_right") and is_on_floor(): #mover der
-		velocity.x += speed
-		$Sprite.flip_h = false
-		$Sprite.frame = 12
-		#$Sprite.frame = 13
-		#$Sprite.frame = 14
-		#$Sprite.frame = 15
+	if is_on_floor() and velocity.x == 0:
+		$AnimatedSprite2D.play("idle") #si no se mueve "idle"
+	elif is_on_floor() and velocity.x < 0 or velocity.x > 0 :
+		$AnimatedSprite2D.play("walk") #si se mueve y esta en el piso "walk"
+	
+	if is_on_floor() and !Input.is_action_pressed("move_right") and !Input.is_action_pressed("move_left"):
+		velocity.x = 0 #si no se toca "a" o "d" no avanza
+	elif is_on_floor() and Input.is_action_pressed("move_right") and Input.is_action_pressed("move_left"):
+		velocity.x = 0 #si se toca "a" y "d" a la vez no avanza
+	elif Input.is_action_pressed("move_left") and is_on_floor():
+		velocity.x -= speed #mover izquierda
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("move_right") and is_on_floor():
+		velocity.x += speed #mover derecha
+		$AnimatedSprite2D.flip_h = false
+	
 	
 	var jump = false #salto
 	if is_on_floor():
 		jump = true
 	
 	if Input.is_action_just_pressed("jump") and jump == true: #salto mínimo
-		velocity.y += min_jump_foce
+		velocity.y += min_jump_force
 		jumping = true
 	
-	
 	#while jumping == true:
-	#	$Sprite.frame = 8
-	#	$Sprite.frame = 9
-	#	$Sprite.frame = 10
-	#	$Sprite.frame = 11
-	
 	
 	if Input.is_action_pressed("jump") and jump == true and jumping == true: #salto hasta el max
-		if velocity.y > max_jump_foce:
+		if velocity.y < max_jump_force:
 			velocity.y -= jump_aceleration * delta 
 		else:
 			jumping = false
@@ -65,8 +59,8 @@ func _physics_process(delta) -> void:
 		jumping = false
 
 	
-	if Input.is_action_pressed("attack") and is_on_floor(): #atacar en el piso
-		velocity.y -= 1 #atacar en aire
+	#if Input.is_action_pressed("attack") and is_on_floor(): #atacar en el piso
+	#	velocity.y -= 1 #atacar en aire
 
 	
 	move_and_slide()
