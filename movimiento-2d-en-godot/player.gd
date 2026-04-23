3a18013b35316f632261ea8d0f76a36fb518f1eb
Rayvet24 <elias.ivan.rodriguez2004@gmail.com>
2026-04-23 16:56:13 -0300

    commit final
    se mejoro mucho el codigo tanto para el movimiento lateral, el salto (se puede saltar poco o mucho) todas las animaciones de mov funcionan bien .El ataque activa animación y colisiones y chekea para saber si golpeo la lampara y asi poder saltar en el aire. 2 bugs, cuando te bajas de una plataforma can_jump sigue siendo true hasta que toque de nuevo el suelo por lo que puede saltar en el aire, y el otro bug es que no salta bien al hacer el salto despues de golpear la lampara

--- movimiento-2d-en-godot/player.gd
+++ movimiento-2d-en-godot/player.gd
@@ -1,66 +1,91 @@
 extends CharacterBody2D
 
-@export var speed = 20
+@export var speed = 325
 @export var gravity = 980
-@export var min_jump_force = -100
-@export var max_jump_force = -500
-@export var jump_aceleration = -250
+@export var jump_time: float = 0.0
+@export var jump_force = 100
 
-var jumping: bool
-
-#func _ready() -> void:
-#	pass
-
+var can_jump: bool #si puede saltar
+var attacking: bool #si está atacando
 # control + k comentar varias lineas
+
+func _on_hitbox_area_entered(area: Area2D) -> void: #si hitbox toca un area como lamp, puede saltar
+	can_jump = true
+	jump_time = 0.0
+
+func check_overlapping_areas():
+	var overlapping = $Hitbox.get_overlapping_areas()
+	for area in overlapping:
+		can_jump = true
+		jump_time = 0.0
 
 func _physics_process(delta) -> void:
 	
-	if not is_on_floor(): #si no esta en el piso se activa gravedad
+	if !is_on_floor(): #si no esta en el piso se activa gravedad
 		velocity.y += gravity * delta 
-	else:
-		velocity.y = 0
-		jumping = false
 	
 	
-	if is_on_floor() and velocity.x == 0:
-		$AnimatedSprite2D.play("idle") #si no se mueve "idle"
-	elif is_on_floor() and velocity.x < 0 or velocity.x > 0 :
-		$AnimatedSprite2D.play("walk") #si se mueve y esta en el piso "walk"
+	var input_dir = Input.get_axis("move_left", "move_right") 
+	#input_dir es una variable, input.get_axis devuelve un numero
+	#hace que move_left sea -1, move_right sea 1 y si no se presiona nada es 0
+	velocity.x = input_dir * speed
+	#al multiplicarlo con velocity hace que la velocidad sea negativa, positiva o 0
 	
-	if is_on_floor() and !Input.is_action_pressed("move_right") and !Input.is_action_pressed("move_left"):
-		velocity.x = 0 #si no se toca "a" o "d" no avanza
-	elif is_on_floor() and Input.is_action_pressed("move_right") and Input.is_action_pressed("move_left"):
-		velocity.x = 0 #si se toca "a" y "d" a la vez no avanza
-	elif Input.is_action_pressed("move_left") and is_on_floor():
-		velocity.x -= speed #mover izquierda
+	
+	#funciona en cualquier caso
+	if velocity.x < 0: #si vel es negativo girar
 		$AnimatedSprite2D.flip_h = true
-	elif Input.is_action_pressed("move_right") and is_on_floor():
-		velocity.x += speed #mover derecha
+	elif velocity.x > 0: #si vel es positivo no girar
 		$AnimatedSprite2D.flip_h = false
 	
 	
-	var jump = false #salto
-	if is_on_floor():
-		jump = true
+	if is_on_floor(): #si esta en el piso puede saltar
+		can_jump = true
+		jump_time = 0.0
+	#si esta en una plataforma is on floor por lo que can jump es true
+	#pero al caer y hasta no tocar de nuevo el suelo sigue siedo true
+	#por lo que puede saltar en el aire
 	
-	if Input.is_action_just_pressed("jump") and jump == true: #salto mínimo
-		velocity.y += min_jump_force
-		jumping = true
 	
-	#while jumping == true:
-	
-	if Input.is_action_pressed("jump") and jump == true and jumping == true: #salto hasta el max
-		if velocity.y < max_jump_force:
-			velocity.y -= jump_aceleration * delta 
+	if Input.is_action_pressed("jump") and can_jump == true: #salto
+		if jump_time < 0.09: #salta hasta que llega al maximo de tiempo de salto
+			velocity.y -= jump_force #aplica fuerza
+			jump_time += delta #suma el tiempo hasta llegar a la coindición
 		else:
-			jumping = false
-
-	if Input.is_action_just_released("jump"): #deja de subir
-		jumping = false
-
+			can_jump = false
+			jump_time = 0.0
 	
-	#if Input.is_action_pressed("attack") and is_on_floor(): #atacar en el piso
-	#	velocity.y -= 1 #atacar en aire
-
+	if Input.is_action_just_released("jump"): #deja de subir
+		can_jump = false
+	
+	
+	if Input.is_action_just_pressed("attack"): #atacar
+		attacking = true
+		if is_on_floor(): #si esta en el piso hace ani de ataque en el piso
+			$AnimatedSprite2D.play("attack_grounded")
+		else: $AnimatedSprite2D.play("attack_air") #sino la anima de ataque en el aire
+	
+	if attacking:
+		if is_on_floor(): velocity.x = 0
+		if !$AnimatedSprite2D.is_playing():
+			attacking = false
+	else: if is_on_floor(): #si no esta atacando
+		if input_dir != 0: #si no esta quieto poner anim de correr
+			$AnimatedSprite2D.play("walk")
+		else: #si esta quieto idle
+			$AnimatedSprite2D.play("idle")
+	else:
+		if velocity.y < 0: #si no esta en piso y sube pone anim de saltar
+			$AnimatedSprite2D.play("jump_start")
+		else: #si no esta en piso y baja anim de cayendo
+			$AnimatedSprite2D.play("jump_end")
+	
+	#$Hitbox/AttackCollision.disabled = true
+	#$ se pone ruta en este caso el nodo y su hijo el cual queremos el metodo
+	
+	if $AnimatedSprite2D.animation == "attack_grounded" or $AnimatedSprite2D.animation == "attack_air":
+		$Hitbox/AttackCollision.disabled = false
+		check_overlapping_areas()
+	else: $Hitbox/AttackCollision.disabled = true
 	
 	move_and_slide()
